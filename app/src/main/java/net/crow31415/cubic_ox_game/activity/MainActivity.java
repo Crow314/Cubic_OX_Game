b22287d0940aaadc8f58b97c7435d3fb6dbba96b
Crow314 <master@crow31415.net>
2019-12-10 12:03:27 +0900

    Create MainActivity (Title)

---
+++ app/src/main/java/net/crow31415/cubic_ox_game/activity/MainActivity.java
@@ -0,0 +1,32 @@
+package net.crow31415.cubic_ox_game.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import net.crow31415.cubic_ox_game.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startLocalGameButton = findViewById(R.id.button_start_local);
+
+        //ボタンイベントリスナー設定
+        //Start
+        startLocalGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), LocalGameActivity.class);
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity(intent);
+            }
+        });
+    }
+}
